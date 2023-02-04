30541c05ac7d29989aaa793c870b1e51d4087d41
Ernesto Figueroa <efigueroa83@hotmail.com>
2023-02-04 07:20:47 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// https://www.youtube.com/watch?v=rLoWMU4L_qE
+// 4 horas 42 min
+
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
+  );
+}
+
+export default App;
